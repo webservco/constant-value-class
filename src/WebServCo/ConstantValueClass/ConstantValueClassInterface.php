ce0ae33ce1ae91bf2faff6ff123199fafe288fd8
Radu Maier <ariademur@webserv.co>
2022-05-18 12:51:52 +0200

    Interface fromValue return interface instead of self

--- src/WebServCo/ConstantValueClass/ConstantValueClassInterface.php
+++ src/WebServCo/ConstantValueClass/ConstantValueClassInterface.php
@@ -7,16 +7,16 @@ namespace WebServCo\ConstantValueClass;
 interface ConstantValueClassInterface
 {
     /**
-    * @param int|string $value
-    */
-    public static function fromValue($value): self;
-
-    /**
     * Return class instance constant value.
     *
     * @return int|string
     */
     public function value();
+
+    /**
+    * @param int|string $value
+    */
+    public static function fromValue($value): \WebServCo\ConstantValueClass\ConstantValueClassInterface;
 
     public function __toString(): string;
 }
